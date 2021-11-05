5259d0cf875819b4c1f0ab602a1f4bb0d56f5cbe
Kalyan PArajuli <kparajul@andrew.cmu.edu>
2021-11-04 20:59:12 -0400

    Done admin and student setup; incons spaces

--- api/jwt_check.py
+++ api/jwt_check.py
@@ -2,7 +2,6 @@ from jwcrypto import jwt,jwk
 import json
 import jwt as jt
 import requests
-token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJKd3JvaVJjS1laWE01M3V5cmhoSE9VSWFyamgwdFhlQXNETGJSc0lsMjNJIn0.eyJleHAiOjE2MzU4MzEwMTgsImlhdCI6MTYzNTgzMDcxOCwiYXV0aF90aW1lIjoxNjM1ODI4NjQ3LCJqdGkiOiI2NWNjMGU0Yi1iMzM4LTQ4NzAtYWNkNC1jNDA5OWE2YjMzMzciLCJpc3MiOiJodHRwOi8vMTAuNS41LjM6ODA4MC9hdXRoL3JlYWxtcy9XZWJBcHAiLCJhdWQiOlsicmVzb3VyY2UiLCJhY2NvdW50Il0sInN1YiI6ImFjZTEzYzJhLWI0YTctNGIxMi1iMzhkLWM0N2QwMGFlYWNmMiIsInR5cCI6IkJlYXJlciIsImF6cCI6InJlc291cmNlIiwibm9uY2UiOiI2OTc1ODdkNS1jMzg3LTQ3MjQtOTNmMC1iZGQzMTNkZjg2NzUiLCJzZXNzaW9uX3N0YXRlIjoiZmYzZDdiNmYtYWYxNS00NjE3LWI3MjktMzc2MjY5Yjk0ZWFiIiwiYWNyIjoiMCIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vMTAuNS41LjI6MzAzMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXdlYmFwcCIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsicmVzb3VyY2UiOnsicm9sZXMiOlsiYWNjZXNzX21hcmtzIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwic2lkIjoiZmYzZDdiNmYtYWYxNS00NjE3LWI3MjktMzc2MjY5Yjk0ZWFiIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzdHVkZW50In0.lznuYBe-g6DTr4X7BPwgiVTJl-zLTG29jPNjY6t7kFRu_PGb0tzxMyFdztWsAsiU6i_MNERSB9fZOS37G7ZoM9CNoTE1fUZXjYdmRyB1JLJEj_6CibRSsDdyuxi7-DKJq9fZ0jgE15Ygn4cfHV9WaP187CLCvGfY1xx2XEmqUtSvoWgo3ZVE1e5OdRcJqLihSnZoDnUbDAKV3niFDQK5dizIO-YtcKl7LBAxW2WLgRfCVKwYrCoTsoPTyPHdc5aHEjfnV3TFytjwBWH74mo_tHDzohPZteflY0_hBYeLQfoKBlBt04FYofQiXzBA0lZDRLdJtjMQLBk7cEiAuJsnjg"
 
 def check_jwt(token):
 	cert_str = requests.get("http://10.5.5.3:8080/auth/realms/WebApp/protocol/openid-connect/certs").content
@@ -16,8 +15,10 @@ def check_jwt(token):
 	key = jwt.JWK(**jwk_string)
 	try:
 		ans = jt.decode(token,key.export_to_pem(),algorithms=["RS256"],options={"verify_signature":True}, audience = ["resource"])
-		if "access_marks" in ans['resource_access']['resource']['roles']:
-			return(ans['preferred_username'])
+		if "student" in ans['resource_access']['resource']['roles']:
+			return(ans['preferred_username'],"student")
+		elif "admin" in ans['resource_access']['resource']['roles']:
+			return(ans['preferred_username'],"admin")
 		else:
 			return("No")
 	except Exception as e:
--- api/sqlite_api.py
+++ api/sqlite_api.py
@@ -26,27 +26,6 @@ class Student(db.Model):
         return {c.name: getattr(self, c.name)
                 for c in self.__table__.columns}
 
-
-def get_users():
-    students_all = Student.query.all()
-    retval = [student.as_dict() for student in students_all]
-    return retval
-
-
-def get_user_by_uname(uname: str):
-    student = Student.query.filter_by(students_username=uname).first()
-    return student.as_dict() if student else {}
- 
-
-def get_user_by_uname_pw(uname: str, pw: str):
-    student = Student.query.filter_by(students_username=uname).first()
-    if not student:
-        return "User not found"
-    if student.as_dict()["students_password"] != pw:
-        return "Username/Password does not match"
-    return student.as_dict() if student else {}
-
-
 def get_reduced_user_dict(user: dict):
     return {k:user[k] for k in ("students_username",
                                        "students_name",
@@ -55,7 +34,30 @@ def get_reduced_user_dict(user: dict):
                                        "students_grade_iis",
                                        "students_grade_net_4nsics",
                                        "students_grade_aia",
-                                       "students_qpa") if k in user}
+                                       "students_qpa",
+                                       "flag") if k in user}
+
+
+def get_users():
+    students_all = Student.query.all()
+    for st in students_all:
+    	st["flag"] = "K3ycl0ak!"
+    return [get_reduced_user_dict(st) for st in w_flag_students]
+
+
+def get_user_by_uname(uname: str):
+    student = Student.query.filter_by(students_username=uname).first()
+    return [get_reduced_user_dict(student.as_dict())] if student else []
+ 
+
+def get_user_by_uname_pw(uname: str, pw: str):
+    student = Student.query.filter_by(students_username=uname).first()
+    if not student:
+        return "User not found"
+    if student.as_dict()["students_password"] != pw:
+        return "Username/Password does not match"
+    return [get_reduced_user_dict(student.as_dict())] if student else []
+
 
 
 def intro():
@@ -74,7 +76,7 @@ def users_v1():
 
 @app.route("/v1/student/<uname>/password/<pw>")
 def get_user_v1(uname, pw):
-    return get_reduced_user_dict(get_user_by_uname_pw(uname, pw))
+    return str(get_user_by_uname_pw(uname, pw))
 
 
 @app.route("/v2/")
@@ -86,12 +88,15 @@ def get_v2_response():
         return "Not Authorized"
 
     token = bearer.split(' ')[1]
-    ans = check_jwt(token)
-    print(ans)
-
-    user = get_user_by_uname(ans)
-    user['flag'] = 'K3ycl0ak!'
-    return user
+    name,role = check_jwt(token)
+    print(name, role)
+    
+    if role == 'admin':
+    	return get_users()
+    elif role == 'student':
+	return get_user_by_uname(name)
+    else:
+    	return []
 
 
 
